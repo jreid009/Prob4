9b3cae230a2f06823feb5f08abe35ed1f2fd5c67
John Reid <jreid009@uab.edu>
2015-04-17 03:11:22 -0500

    Pro Series 4
    2 layer dropdown

---
+++ js/Assignment5.js
@@ -0,0 +1,20 @@
+function MenuChoice()
+{
+    if (document.getElementById("menu").value == "Display Show Area 1")
+    {
+        document.getElementById("showarea1").style.visibility = "visible";
+        document.getElementById("showarea2").style.visibility = "hidden";
+    }
+    else if (document.getElementById("menu").value == "Display Show Area 2")
+    {
+        document.getElementById("showarea1").style.visibility = "hidden";
+        document.getElementById("showarea2").style.visibility = "visible";
+    }
+    else
+    {
+        document.getElementById("showarea1").style.visibility = "hidden";
+        document.getElementById("showarea2").style.visibility = "hidden";
+    }
+}
+
+
